0d6cf2a5fe959e1fc5086dd5d86e31ab2fcb906e
Neha Desai <nehadesai247@gmail.com>
2023-10-02 22:11:49 -0700

    Add functions, refactor code, add variables
    Add function to get player selection, add variables to keep track of
    number of wins/ties, refactor oneRound function, add function to
    display who won overall, add function which allows the user to play
    five rounds of the game and displays individual round winner and
    overall winner

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
+function getPlayerSelection() {
+    return prompt("Make a choice: Rock, Paper, or Scissors");
+}
+
 function getComputerChoice() {
     let arr = ["Rock", "Paper", "Scissors"];
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+let playerWins = 0;
+let computerWins = 0;
+let numberTies = 0;
+
 function oneRound(playerSelection, computerSelection) {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
 
     if (playerSelection === computerSelection) {
+        numberTies += 1;
         return `Draw! You both chose ${playerSelection}`;
     }
+
     else if (playerSelection === "Rock") {
-        return (computerSelection === "Scissors") ? "You Win! Rock beats Scissors" : "You Lose! Paper beats Rock";
+
+        if(computerSelection === "Scissors") {
+            playerWins += 1;
+            return "You Win! Rock beats Scissors";
+        }
+        else {
+            computerWins += 1;
+            return "You Lose! Paper beats Rock";
+        }
     }
+
     else if (playerSelection === "Paper") {
-        return (computerSelection === "Rock") ? "You Win! Paper beats Rock" : "You Lose! Scissors beats Paper";
+
+        if (computerSelection === "Rock") {
+            playerWins += 1;
+            return "You Win! Paper beats Rock";
+        }
+
+        else {
+            computerWins += 1;
+            return "You Lose! Scissors beats Paper";
+        }
+        
+    }
+
+    else {
+
+        if(computerSelection === "Paper") {
+            playerWins += 1;
+            return "You Win! Scissors beats Paper";
+        }
+        
+        else {
+            computerWins += 1;
+            return "You Lose! Rock beats Scissors";
+        }
+
+    }
+}
+
+function whoWonMore() {
+    if (playerWins === computerWins) {
+        return "Tie Overall!";
+    }
+    else if (playerWins > computerWins) {
+        return `You won overall! You won ${playerWins}/5 games and tied ${numberTies} times!`;
     }
     else {
-        return (computerSelection === "Paper") ? "You Win! Scissors beats Paper" : "You Lose! Rock beats Scissors";
+        return `You lost overall! You won ${playerWins}/5 games though and tied ${numberTies} times!`;
     }
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(oneRound(getPlayerSelection(), getComputerChoice()));
+    }
+    console.log(whoWonMore());
 }
